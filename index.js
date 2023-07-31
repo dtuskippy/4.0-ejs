e7b9073b075a1a60fa7a9ca695f80f20a3b97ed9
Alan Chelko <achelko@gmail.com>
2023-07-31 16:12:58 +0000

    Final edits.

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import ejs from "ejs";
 const app = express();
 const port = 3000;
 
-const d = new Date();
-let day = d.getDay()
-let sect1 = "";
-let sect2 = ""; 
+app.get('/', (req, res) => {
+  const d = new Date();
+  let day = d.getDay()
+  let sect1 = "";
+  let sect2 = ""; 
 
-function figureTheDay(day) {
   if(day > 0 && day < 6) {
     sect1 = "a weekday";
     sect2 = "work hard";
@@ -17,23 +17,10 @@ function figureTheDay(day) {
     sect1 = "the weekend";
     sect2 = "have fun";
   }
- 
-}
-
-app.use(figureTheDay);
 
 
-app.get('/', (req, res) => {
   res.render("index.ejs", {sect1: sect1, sect2: sect2});
 });
-
-// app.post...
-// res.render("index.ejs",
-// {name: req.body.name}
-// });
-
-
-
 
 
 
